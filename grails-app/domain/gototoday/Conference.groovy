853177683ae3a2259ac6cee93a4b485ad75b693c
Martin Westergaard Lassen <martin@mwl.dk>
2011-10-13 08:10:44 +0200

    Created Post

--- grails-app/domain/gototoday/Conference.groovy
+++ grails-app/domain/gototoday/Conference.groovy
@@ -4,6 +4,8 @@ class Conference {
     String name
     String urlTag
 
+    static hasMany = [posts: Post]
+
     static constraints = {
         name(blank: false)
         urlTag(blank: false, matches: "[a-z\\-0-9]+")
