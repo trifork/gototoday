740b40556d7f9af6df08334839f937c444974740
Martin Westergaard Lassen <martin@mwl.dk>
2011-10-24 16:01:27 +0200

    added ErrorsControllerTest

---
+++ test/unit/gototoday/ErrorsControllerTests.groovy
@@ -0,0 +1,17 @@
+package gototoday
+
+import grails.test.*
+
+class ErrorsControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
